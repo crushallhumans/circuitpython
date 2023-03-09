93de67b851d99b7007e001883061eb59e4718a35
Conrad Rushing <conrad.rushing@gmail.com>
2023-03-08 23:10:17 -0500

    Made util pipe-ready

--- life_solcube/utils/imagemagick_pixel_output_retranslator.py
+++ life_solcube/utils/imagemagick_pixel_output_retranslator.py
@@ -1,5 +1,5 @@
 #For converting pixel thresholding calc from ImageMagick:
-#convert ~/Downloads/0f865be4c6b6330.gif -threshold 50% -depth 1 txt:
+#➜ convert 0f865be4c6b6330.gif -threshold 50% -depth 1 txt: | python3 ./imagemagick_pixel_output_retranslator.py
 # ex: 
 # 27,63: (255,255,255)  #FFFFFF  white
 # 28,63: (255,255,255)  #FFFFFF  white
@@ -11,8 +11,14 @@
 import pprint as pp
 import re
 
-input_set = open("pawpatrol_intro_pixels.txt", 'r').read().splitlines() 
+import sys
 
+#input_set = open("pawpatrol_intro_pixels.txt", 'r').read().splitlines() 
+input_set = []
+for line in sys.stdin:
+    input_set.append(line)
+
+valid_line = re.compile('^[0-9]')
 matcher = re.compile('\(0')
 
 #print(input_set)
@@ -20,7 +26,8 @@ px = []
 for i in range(0,64):
     px.append(['.'] * 64)
 for i in input_set:
-    #print(i)
+    if not re.match(valid_line,i):
+        continue
     vals = i.split(': ')
     coords = vals[0].split(',')
     pixel = ' '
