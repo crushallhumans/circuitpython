f66be071fef6f0a9aea89d2c9469aba71bae6369
Conrad Rushing <conrad.rushing@gmail.com>
2023-07-13 13:11:18 -0400

    Matrixportal experiments

---
+++ matrixportal/secrets.py
@@ -0,0 +1,11 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'styx',
+    'password' : 'broom leaping final heat',
+    'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
+    'github_token' : 'foobarbaz',
+    'aio_username': 'crushallhumans',
+    'aio_key': '64e07a37547145a38bf87dc8492140e8'
+    }
